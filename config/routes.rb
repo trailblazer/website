14649c7fc23f70609facf15b44ff75a05f95cd43
Yogesh Khater <yogeshjain999@gmail.com>
2022-07-10 12:04:11 +0530

    Setup 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get "/2.1/:md_name", to: "pages#show"
   get "/2.1", to: "home#show"
 
+  # 2.0 routes
+  get "/gems/(:name)", to: redirect("/2.0/gems/%{name}")
+
   root "home#show"
 end
