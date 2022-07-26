eaab3590320fb6a3f19421db658d866752a72146
Yogesh Khater <yogeshjain999@gmail.com>
2022-07-26 22:48:21 +0530

    Use vite_image_tag

--- config/initializers/torture_server.rb
+++ config/initializers/torture_server.rb
@@ -6,6 +6,10 @@ Snippets.class_eval do
   def api(path)
     %(<a href="#{path}"><i class="fa fa-book" aria-hidden="true"></i> API docs</a>)
   end
+
+  def img(path)
+    raise "Please replace `img` with `vite_image_tag`"
+  end
 end
 
 Torture::Page::HTML.class_eval do
