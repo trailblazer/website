f8997cd237d8e681bc701e94126ff4195b23e31f
Yogesh Khater <yogeshjain999@gmail.com>
2022-07-31 18:00:48 +0530

    Do not remove public/2.1

--- app/concepts/application/operation/compile.rb
+++ app/concepts/application/operation/compile.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class Application::Operation::Compile < Trailblazer::Operation
-  step :remove_old_files
   step Subprocess(Home::Operation::Compile)
   step Subprocess(Documentation::Operation::Compile)
   step Subprocess(Page::Operation::Compile)
-
-  def remove_old_files(_ctx, dist_path: "public", **)
-    FileUtils.rm_rf("#{dist_path}/2.1")
-  end
 end
