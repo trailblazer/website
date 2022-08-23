160e4410e55c195b3f790ef868e40cd6022d0e72
Nick Sutterer <apotonick@gmail.com>
2022-08-23 12:18:21 +0200

    Publishing public/dist

--- public/dist/vite/assets/page.22caf2d9.js
+++
@@ -1,2 +0,0 @@
-import{j as a,H as r,r as g,x as e,a as i}from"./main.856e24d3.js";import"./initAnchor.997783a9.js";a(document).ready(function(){r.registerLanguage("ruby",g),r.registerLanguage("xml",e),r.registerLanguage("json",i),r.highlightAll()});
-//# sourceMappingURL=page.22caf2d9.js.map
--- public/dist/vite/assets/page.f2cdcd5d.js
+++
@@ -1,2 +0,0 @@
-import{j as r,H as a,r as e,x as g,a as s}from"./main.5d285120.js";import"./initAnchor.504531b1.js";r(document).ready(function(){a.registerLanguage("ruby",e),a.registerLanguage("xml",g),a.registerLanguage("json",s),a.highlightAll()});
-//# sourceMappingURL=page.f2cdcd5d.js.map
