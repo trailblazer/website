a83bc73b14a6c45c5510ca98c7361a7e629748b0
Nick Sutterer <apotonick@gmail.com>
2022-10-06 18:11:24 +0200

    Publishing public/dist

--- public/dist/vite/assets/page.a508a6bd.js
+++
@@ -1,2 +0,0 @@
-import{j as a,H as r,r as g,x as e,a as s}from"./main.add27b08.js";import"./initAnchor.6c3fbdd0.js";a(document).ready(function(){r.registerLanguage("ruby",g),r.registerLanguage("xml",e),r.registerLanguage("json",s),r.highlightAll()});
-//# sourceMappingURL=page.a508a6bd.js.map
